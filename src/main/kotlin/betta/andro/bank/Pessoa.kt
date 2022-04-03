214d500e185941eb92fa13e79e0bc2463285b318
carlos-devico <carlosdevico@gmail.com>
2022-04-03 16:03:43 -0300

    conceito de inner class aplicado para acesso do atributo rua da classe endereco

--- src/main/kotlin/betta/andro/bank/Pessoa.kt
+++ src/main/kotlin/betta/andro/bank/Pessoa.kt
@@ -3,6 +3,10 @@ package betta.andro.bank
 class Pessoa {
     var nome: String = "carlos"
     var cpf: String = "123.456.789-22"
+
+    inner class Endereco{
+        var rua: String = "Rua teste"
+    }
 }
 
 fun main() {
@@ -10,5 +14,6 @@ fun main() {
 
     println(carlos.nome)
     println(carlos.cpf)
+    println(carlos.Endereco().rua)
 
 }
