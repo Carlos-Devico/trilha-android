da8939e73a725ea8797dfb8e3743b20990ff427d
carlos-devico <carlosdevico@gmail.com>
2022-04-03 16:49:49 -0300

    Criado classe Banco

---
+++ src/main/kotlin/betta/andro/bank/Banco.kt
@@ -0,0 +1,9 @@
+package betta.andro.bank
+
+data class Banco(val nome: String,val numero: Int){
+
+//    https://web.dio.me/course/fundamentos-de-orientacao-a-objetos-com-kotlin/learning/a12f4db5-ae24-4c7a-914a-e883f53f9b20?back=/browse
+//    navegador devico
+
+
+}
